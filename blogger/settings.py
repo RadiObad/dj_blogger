2f8fbef38ada423ed5cf19a5ddc051e809b5b4ae
Radi Obad <lovelovelove67@gmail.com>
2020-06-09 17:21:26 +0300

    fix complete sender email

--- blogger/settings.py
+++ blogger/settings.py
@@ -130,9 +130,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = os.environ.get('lovelovelvoe67@gmail.com')
-# EMAIL_HOST_PASSWORD = os.environ.get('obad@12345')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'omradi136@gmail.com'
+EMAIL_HOST_PASSWORD = 'jfhifqnvceduygnc'
