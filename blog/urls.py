3463b19dafb8060bf5ca6c88652c8c65206c3f70
Radi Obad <lovelovelove67@gmail.com>
2020-06-10 16:13:04 +0300

    fix url

--- blog/urls.py
+++ blog/urls.py
@@ -11,7 +11,7 @@ from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
-    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('user/<str:username>',UserPostListView.as_view(), name='user-posts'),
     path('post/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<slug:slug>/update/', PostUpdateView.as_view(), name='post-update'),
