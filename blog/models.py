3f989045372019e041b38ce77cd5ae193c89b0b0
Radi Obad <lovelovelove67@gmail.com>
2020-06-10 23:54:23 +0300

    no hing new

--- blog/models.py
+++ blog/models.py
@@ -4,14 +4,37 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.title
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
+class Post(models.Model):
+	STATUS_CHOICES = (
+		('draft', 'Draft'),
+		('published', 'Published'),
+	)
+	title = models.CharField(max_length=250)
+	slug = models.SlugField(max_length=250, unique_for_date='published')
+	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+	content = models.TextField()
+	publish = models.DateTimeField(default=timezone.now)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	status = models.CharField(max_length=10,
+								choices=STATUS_CHOICES,
+								default='draft')
+	
+
+	class Meta:
+		ordering = ('-publish',)								
+	
+	def __str__(self):
+		return self.title
     
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
+	def get_absolute_url(self):
+ 		return reverse('post-detail', kwargs={'pk': self.pk})
+	
+	objects = models.Manager() # The default manager.
+	published = PublishedManager() # Our custom manager.
--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
-    ordering = ['-date_posted']
+    ordering = ['-publish']
     paginate_by = 5
 
 
@@ -34,7 +34,7 @@ class UserPostListView(ListView):
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
-        return Post.objects.filter(author=user).order_by('-date_posted')
+        return Post.objects.filter(author=user).order_by('-publish')
 
 
 class PostDetailView(DetailView):
