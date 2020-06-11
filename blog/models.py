89850c6640728ef367d9cb205594fbf24a13176d
Radi Obad <lovelovelove67@gmail.com>
2020-06-11 17:08:11 +0300

    some oroblem fix

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.db.models.signals import pre_save
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -5,7 +6,8 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -14,4 +16,17 @@ class Post(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
+        return reverse('post-detail', kwargs={'slug': self.slug})
+
+
+
+from .utils import unique_slug_generator
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        # instance.slug = create_slug(instance)
+        instance.slug = unique_slug_generator(instance)
+
+
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
